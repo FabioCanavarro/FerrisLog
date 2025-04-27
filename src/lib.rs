16f96a173275bd9ceac165c4da0e7b1b3e75070a
FabioCanavarro <fabiocanavarrotoh@gmail.com>
2025-04-27 18:11:44 +0700

    Where i left from the base project

---
+++ src/lib.rs
@@ -0,0 +1,11 @@
+extern crate chrono;
+extern crate clap;
+extern crate serde;
+extern crate serde_json;
+extern crate slog_term;
+extern crate bincode;
+
+#[macro_use]
+extern crate slog;
+
+pub mod kvstore;
