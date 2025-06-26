8455ec20ed0d8c1feffce338e396ead182c6fd7e
FabioCanavarro <fabiocanavarrotoh@gmail.com>
2025-06-26 15:11:37 +0700

    initial code

---
+++ benches/get.rs
@@ -0,0 +1,29 @@
+use criterion::{black_box, criterion_group, criterion_main, Criterion};
+use ferris::kvstore::KvStore;
+use tempfile::TempDir;
+
+fn fake_data() -> (String,String){
+    (rand::random::<i32>().to_string(),rand::random::<i32>().to_string())
+}
+
+
+pub fn criterion_benchmark(c: &mut Criterion) {
+    let (key, value) = fake_data();
+    c.bench_function("Set random", 
+        |b| b.iter_batched(
+                || {
+                    let temp_dir = TempDir::new().unwrap();
+                    let store = KvStore::open(temp_dir.path()).unwrap();
+                    (store,temp_dir)
+                },
+                //NOTE: I kept tempdir, because, they keep dropping it after the setup finish which
+                //cause it fo fail, cuz tempdir is dropped
+                |(mut store, _tempdir)| store.set(black_box(key.clone()), black_box(value.clone())).unwrap(),
+                criterion::BatchSize::SmallInput
+        )
+    );
+}
+
+criterion_group!(benches, criterion_benchmark);
+criterion_main!(benches);
+
