2f179bea44e278abe3b6330cff7202b9818afebe
FabioCanavarro <fabiocanavarrotoh@gmail.com>
2025-06-29 13:46:28 +0700

    made the worker return a JoinHandle<()>

--- src/concurrency/naive.rs
+++ src/concurrency/naive.rs
@@ -1,16 +1,16 @@
-use std::{clone, env::Args, fmt::Debug, sync::{mpsc::{self, channel, Receiver, Sender}, Arc, Mutex}, thread::{self, JoinHandle}};
+use std::{fmt::Debug, sync::{mpsc::{self, channel, Receiver, Sender}, Arc, Mutex}, thread::{self, JoinHandle}};
 use crate::kvstore::error::KvResult;
 
 use super::ThreadPool;
 
 #[derive(Debug)]
-struct Worker<T>{
-    thread: JoinHandle<T>
+struct Worker{
+    thread: JoinHandle<()>
     
 }
 
-impl<T: Send + 'static> Worker<T> {
-    pub fn spawn<F: FnOnce() + Send + 'static>(rx: Arc<Mutex<Receiver<F>>>) -> Worker<T>{
+impl Worker {
+    pub fn spawn<F: FnOnce() + Send + 'static>(rx: Arc<Mutex<Receiver<F>>>) -> Worker{
         let handle = thread::spawn(
             move|| {
                 loop {
@@ -24,14 +24,14 @@ impl<T: Send + 'static> Worker<T> {
 }
 
 #[derive(Debug)]
-pub struct SharedQueueThreadPool<T,F: Send + FnOnce() + 'static> {
-    workers: Vec<Worker<T>>,
+pub struct SharedQueueThreadPool<F: Send + FnOnce() + 'static> {
+    workers: Vec<Worker>,
     channel: (Sender<F>, Arc<Mutex<Receiver<F>>>)
 
 }
 
-impl<T: Send + 'static, F: FnOnce() + Send + 'static> ThreadPool<F> for SharedQueueThreadPool<T, F> {
-    fn new (n: i32) -> KvResult<SharedQueueThreadPool<T,F>> {
+impl<F: FnOnce() + Send + 'static> ThreadPool<F> for SharedQueueThreadPool<F> {
+    fn new (n: i32) -> KvResult<SharedQueueThreadPool<F>> {
         let mut workers = vec![];
         let (sx, rx) = channel();
         let rx = Arc::new(Mutex::new(rx));
